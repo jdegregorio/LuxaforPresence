c810a9346e396c6ee3feafed18725b6cf0748dd6
jdegregorio <joseph.degregorio@gmail.com>
2026-07-12 15:31:08 -0700

    test: restore baseline engine expectations

--- LuxaforPresence/Tests/PresenceEngineTests.swift
+++ LuxaforPresence/Tests/PresenceEngineTests.swift
@@ -50,7 +50,7 @@ final class PresenceEngineTests: XCTestCase {
 
         engine.tick()
 
-        XCTAssertEqual(lux.actions, [.off(config.remoteWebhookUserId), .off(config.remoteWebhookUserId)])
+        XCTAssertEqual(lux.actions, [.off(config.remoteWebhookUserId)])
     }
 
     func testTick_transitionsToInMeeting_whenCameraActive_evenIfMeetingDetectorInactive() {
@@ -179,7 +179,7 @@ final class PresenceEngineTests: XCTestCase {
         front.isMeetingApp = true
         engine.tick()
 
-        XCTAssertEqual(lux.actions, [.off(config.remoteWebhookUserId)])
+        XCTAssertEqual(lux.actions, [.off(config.remoteWebhookUserId), .off(config.remoteWebhookUserId)])
     }
 
     func testTick_meetingActiveAndVadSilentBeyondGrace_turnsYellow() {
